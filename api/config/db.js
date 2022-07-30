c5ef776ec5928fce1774bed07c0d9918a25b5262
Mouhamed <mouhamed-el-moustapha.mboup@epitech.eu>
2022-07-30 04:50:21 +0200

    Configuration

---
+++ api/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://" +process.env.DB_USER_PASS + "@cluster0.wcpbp.mongodb.net/mern-sm?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB", err));
--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+require('dotenv').config({path: "./config/.env"});
+require('./config/db');
+
 const app = express();
 
 
 
-app.listen(5000, () => {
-    console.log('Listening on port 5000');
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
 })
