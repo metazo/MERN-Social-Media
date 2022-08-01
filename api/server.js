926948278b6c07675491cb39789e078ba86a1fa2
metazo <metaz0412@gmail.com>
2022-08-01 13:45:07 +0200

    Initial commit

--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const userRoute = require('./routes/user.routes')
 require('dotenv').config({path: "./config/.env"});
 require('./config/db');
 
 const app = express();
 
 
+//Routes
+app.use('/api/user', userRoute);
 
+
+//Serveur
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
 })
