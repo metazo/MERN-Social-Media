d11e256c9b53c13d0393b594e530e094b4bcaace
Mouahamed Mboup <mouhamed-el-moustapha.mboup@epitech.eu>
2022-10-07 20:15:21 +0200

    Mise a jour de la DB config

--- api/config/db.js
+++ api/config/db.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose
-  .connect(
-    "mongodb+srv://" +process.env.DB_USER_PASS + "@cluster0.wcpbp.mongodb.net/mern-sm?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log("Failed to connect to MongoDB", err));
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.wcpbp.mongodb.net/mern-sm?retryWrites=true&w=majority`,
+  () => {
+    console.log("connected to mongodb");
+  }
+);
